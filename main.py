d1b3bb5b9d1a2c36801c822c11c52dd62b6cdece
dutenrapha <dutenrapha@hotmail.com>
2023-10-23 21:25:20 -0300

    Add endpoint flask

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
+from flask import Flask, request, render_template
 from rotas import obter_rota_acessivel
 
-# Exemplo de uso
-origem = "rua joão luis vives 42, São Paulo"
-destino = "Av. da Liberdade 532, São Paulo"
+app = Flask(__name__)
 
-rota_acessivel = obter_rota_acessivel(origem, destino)
-if rota_acessivel:
-    print("Rota acessível:")
-    for step in rota_acessivel["legs"][0]["steps"]:
-        print(step["html_instructions"])
-else:
-    print("Não foi possível obter a rota acessível.")
-   
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    solution = None
+    if request.method == 'POST':
+
+      origem  = request.form.get('origem')
+      destino  = request.form.get('destino')
+       
+      solution = obter_rota_acessivel(origem, destino)
+      solution = [step["html_instructions"] for step in solution["legs"][0]["steps"]]
+
+      return render_template('index.html', solution=solution)
+    else:
+        return render_template('index.html', solution=solution)
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# Exemplo de uso
+# origem = "rua joão luis vives 42, São Paulo"
+# destino = "Av. da Liberdade 532, São Paulo"
